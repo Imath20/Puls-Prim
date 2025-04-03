dadcc110f7d6cae04e20a5b56acd72d74ad6ad94
FujisanRealEstate <154996184+Imath20@users.noreply.github.com>
2025-04-04 00:18:23 +0300

    2

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-// scripts.js
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.getElementById('hamburger');
-    const mobileMenu = document.getElementById('mobileMenu');
-
-    hamburger.addEventListener('click', function() {
-        mobileMenu.classList.toggle('active');
-    });
-});
